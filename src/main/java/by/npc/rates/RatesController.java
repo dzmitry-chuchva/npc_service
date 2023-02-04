b9c6aab814e1a1f105d4ebd6e59cae26027ab151
Dzmitry Chuchva <realmitro@gmail.com>
2023-02-04 18:50:27 +0300

    actually, it makes no sense to use @ControllerAdvice to handle exception for one and only controller
    ...so just inline all the exception handlers.

--- src/main/java/by/npc/rates/RatesController.java
+++ src/main/java/by/npc/rates/RatesController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
@@ -52,34 +51,31 @@ public class RatesController {
         throw new RateNotFoundException(date, abbr);
     }
 
-    @RestControllerAdvice(assignableTypes = RatesController.class)
-    public static class RatesExceptionHandler {
-        @ExceptionHandler(RatesReadinessException.class)
-        @ResponseStatus(HttpStatus.BAD_REQUEST)
-        public String ratesReadiness(RatesReadinessException e) {
-            log.error("Rates not ready", e);
-            return "No rates loaded yet for " + e.getOnDate();
-        }
+    @ExceptionHandler(RatesReadinessException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String ratesReadiness(RatesReadinessException e) {
+        log.error("Rates not ready", e);
+        return "No rates loaded yet for " + e.getOnDate();
+    }
 
-        @ExceptionHandler(RateNotFoundException.class)
-        @ResponseStatus(HttpStatus.NOT_FOUND)
-        public String ratePresence(RateNotFoundException e) {
-            log.info("Rate not found", e);
-            return "Rate for " + e.getAbbr() + " doesn't exist for " + e.getDate();
-        }
+    @ExceptionHandler(RateNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String ratePresence(RateNotFoundException e) {
+        log.info("Rate not found", e);
+        return "Rate for " + e.getAbbr() + " doesn't exist for " + e.getDate();
+    }
 
-        @ExceptionHandler(RestClientResponseException.class)
-        @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-        public String unexpectedRemoteAPIResponse(RestClientResponseException e) {
-            log.error("Unexpected response of remote API received. API response status was: {}", e.getStatusText());
-            return "Remote API response was not what we expected. The status code was: [" + e.getStatusText() + "]";
-        }
+    @ExceptionHandler(RestClientResponseException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String unexpectedRemoteAPIResponse(RestClientResponseException e) {
+        log.error("Unexpected response of remote API received. API response status was: {}", e.getStatusText());
+        return "Remote API response was not what we expected. The status code was: [" + e.getStatusText() + "]";
+    }
 
-        @ExceptionHandler(Exception.class)
-        @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-        public String unexpected(Exception e) {
-            log.error("Unexpected error occurred", e);
-            return "Unexpected error has occurred. Associated message: [" + e.getMessage() + "]";
-        }
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String unexpected(Exception e) {
+        log.error("Unexpected error occurred", e);
+        return "Unexpected error has occurred. Associated message: [" + e.getMessage() + "]";
     }
 }
